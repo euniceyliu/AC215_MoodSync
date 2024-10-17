8f5ea4cae9e78a8e03fe21a8925fee9700cbcf65
Xinyu <0v0smiling@gmail.com>
2024-10-17 19:46:35 -0400

    container running for datapipeline

--- src/datapipeline/docker-shell.sh
+++ src/datapipeline/docker-shell.sh
@@ -7,7 +7,8 @@ set -e
 export BASE_DIR=$(pwd)
 export PERSISTENT_DIR=$(pwd)/../persistent-folder/
 export SECRETS_DIR=$(pwd)/../secrets/
-export GCP_PROJECT="ac215-mood-sync" # CHANGE TO YOUR PROJECT ID
+export GCS_BUCKET_NAME="prompt-playlist-data"
+export GCP_PROJECT="ac215-project-438523" # CHANGE TO YOUR PROJECT ID
 export GOOGLE_APPLICATION_CREDENTIALS="/secrets/llm-service-account.json"
 export IMAGE_NAME="llm-rag-cli"
 
@@ -20,8 +21,18 @@ docker network inspect llm-rag-network >/dev/null 2>&1 || docker network create 
 # Build the image based on the Dockerfile
 docker build -t $IMAGE_NAME -f Dockerfile .
 
+# # Run Container
+docker run --rm --name $IMAGE_NAME -ti \
+-v "$BASE_DIR":/app \
+-v "$SECRETS_DIR":/secrets \
+-v "$PERSISTENT_DIR":/persistent \
+-e GOOGLE_APPLICATION_CREDENTIALS=$GOOGLE_APPLICATION_CREDENTIALS \
+-e GCP_PROJECT=$GCP_PROJECT \
+$IMAGE_NAME
+
+
 # Run All Containers
-docker compose run --rm --service-ports $IMAGE_NAME
+#docker compose run --rm --service-ports $IMAGE_NAME
 #docker run --rm -ti -v "$(pwd)":/app $IMAGE_NAME
 
 ### something is not found with this
