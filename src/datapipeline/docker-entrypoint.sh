1ee1dc08fa43c570fd84cbeff06fd586bac07ec0
Xinyu <0v0smiling@gmail.com>
2024-11-05 17:55:46 -0500

    some changes to accommodate dvc

--- src/datapipeline/docker-entrypoint.sh
+++ src/datapipeline/docker-entrypoint.sh
@@ -5,6 +5,14 @@ echo "Container is running!!!"
 args="$@"
 echo $args
 
+gcloud auth activate-service-account --key-file=$GOOGLE_APPLICATION_CREDENTIALS_data
+mkdir -p /mnt/gcs_bucket
+gcsfuse --key-file=$GOOGLE_APPLICATION_CREDENTIALS_data $GCS_BUCKET_NAME /mnt/gcs_data
+echo 'GCS bucket mounted at /mnt/gcs_data'
+mkdir -p /app/output
+mount --bind /mnt/gcs_data/output /app/output
+
+
 if [[ -z ${args} ]]; 
 then
     pipenv shell
--- src/datapipeline/docker-shell.sh
+++ src/datapipeline/docker-shell.sh
@@ -9,6 +9,7 @@ export PERSISTENT_DIR=$(pwd)/../persistent-folder/
 export SECRETS_DIR=$(pwd)/../secrets/
 export GCP_PROJECT="ac215-project-438523" # CHANGE TO YOUR PROJECT ID
 export GOOGLE_APPLICATION_CREDENTIALS="/secrets/llm-service-account.json"
+export GOOGLE_APPLICATION_CREDENTIALS_data="/secrets/data-service-account.json"
 export IMAGE_NAME="llm-rag-cli"
 export GCS_BUCKET_NAME="rag_data_song" 
 export GCP_ZONE="us-central1-a"
@@ -18,9 +19,11 @@ export GCP_ZONE="us-central1-a"
 docker network inspect llm-rag-network >/dev/null 2>&1 || docker network create llm-rag-network
 
 # Build the image based on the Dockerfile
+echo "Building image"
 docker build -t $IMAGE_NAME -f Dockerfile .
 
 # Run All Containers
+echo "Running container"
 docker compose run --rm --service-ports $IMAGE_NAME
 
 
