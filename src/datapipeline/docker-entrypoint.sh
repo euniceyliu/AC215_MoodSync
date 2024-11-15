7f6d5684af7b9847a55ab479030227c53b1b65e2
Xinyu <0v0smiling@gmail.com>
2024-11-14 19:27:45 -0500

    dvc update

--- src/datapipeline/docker-entrypoint.sh
+++ src/datapipeline/docker-entrypoint.sh
@@ -9,8 +9,11 @@ gcloud auth activate-service-account --key-file=$GOOGLE_APPLICATION_CREDENTIALS_
 mkdir -p /mnt/gcs_bucket
 gcsfuse --key-file=$GOOGLE_APPLICATION_CREDENTIALS_data $GCS_BUCKET_NAME /mnt/gcs_data
 echo 'GCS bucket mounted at /mnt/gcs_data'
-mkdir -p /app/output
-mount --bind /mnt/gcs_data/output /app/output
+rm -r /app/output
+mkdir /app/output
+#mount --bind /mnt/gcs_data/output /app/output
+#gsutil ls gs://rag_data_song/  #this works BUT mount --bind doesn't with gcs FOR WHAETEVER REASON, let's work around it then
+gsutil -m cp gs://rag_data_song/output/* /app/output/
 
 
 if [[ -z ${args} ]]; 
